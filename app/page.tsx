037dcb805a0987565d795047ba5317cff3ee5507
stormymayday <stormymayday7@gmail.com>
2024-11-21 22:55:47 -0800

    Import Data, Setup State Variable, Iterate and Render, Clear List

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,22 @@
+"use client";
+
+import { data } from "@/app/data.js";
+import { useState } from "react";
+
 export default function Home() {
-    return <div>Birthday Reminder</div>;
+    const [people, setPeople] = useState(data);
+    const handleClick = () => {
+        setPeople([]);
+    };
+    return (
+        <>
+            {people.map((person) => {
+                const { id, name } = person;
+                return <div key={id}>{name}</div>;
+            })}
+            <button type="button" className="btn" onClick={handleClick}>
+                clear
+            </button>
+        </>
+    );
 }
